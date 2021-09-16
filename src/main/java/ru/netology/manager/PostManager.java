d32c0d83407f73c807d41f4f4308e6f2fe898573
Victroria Anisimova <vitkakim@mail.ru>
2021-09-16 20:35:14 +0300

    add PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,13 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset){
+        return null;
+    }
+
+    public void delete(int ownerId, int postId){
+
+    }
+}
